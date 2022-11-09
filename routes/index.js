a4f6c2dd9c5cccb3bbd719b64e0c49201245979d
Salvador Beltran <salbeltran1995@gmail.com>
2022-11-08 19:13:12 -0500

    server index dateFormat

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,10 @@
-// Require express router
 const router = require('express').Router();
-
-// Import all of the API routes 
 const apiRoutes = require('./api');
 
-// add prefix of `/api` to all of the api routes
 router.use('/api', apiRoutes);
 
-// 404 Status error message
 router.use((req, res) => {
-    res.status(404).send('<h1>404 Error....</h1>');
-  });
+  res.status(404).send('<h1>routes not working in index.js</h1>')
+})
 
-// Module exports router
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-// Require express and mongoose
-const express = require('express');
-const mongoose = require('mongoose');
+	// modules to require
+	const express = require('express');
+	const mongoose = require('mongoose');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+	const app = express();
+	const PORT = process.env.PORT || 5001;
 
-app.use(express.static('public'));
+	// express middleware
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
+	// use the routes folder for routing
+	app.use(require('./routes'));
 
-// Connect mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+	// connect to mongoose
+	mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
 
-// Log mongoose queries
-mongoose.set('debug', true);
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
 
-app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
+	// ('debug', true) logs  the mongoose queries to the console
+	mongoose.set('debug', true);
+
+	// listen on the port
+	app.listen(PORT, () => console.log(`Connected to Social-Network-API on http://localhost:${PORT}/`));
